3bff2c60672cfb90a70d0bc2bbafb19070ebaf47
Nicolas Ramel <nramel@r-numerique.fr>
2019-03-19 19:06:56 +0100

    Update menu.php

--- config/menu.php
+++ config/menu.php
@@ -1,29 +1,11 @@
 <?php
 
 /********** CONFIG MENU ************/
-$tabMenu = array () ;
-$tabMenu["id"] = "com_ze_apps_opportunity_activities" ;
-$tabMenu["space"] = "com_ze_apps_config" ;
-$tabMenu["label"] = "Activités" ;
-$tabMenu["fa-icon"] = "activity" ;
-$tabMenu["url"] = "/ng/com_zeapps/opportunity_activities" ;
-$tabMenu["access"] = "com_zeapps_contact_read" ;
-$tabMenu["order"] = 39 ;
-$menuLeft[] = $tabMenu ;
 
-$tabMenu = array () ;
-$tabMenu["id"] = "com_ze_apps_opportunity_status" ;
-$tabMenu["space"] = "com_ze_apps_config" ;
-$tabMenu["label"] = "Status" ;
-$tabMenu["fa-icon"] = "signal" ;
-$tabMenu["url"] = "/ng/com_zeapps/opportunity_status" ;
-$tabMenu["access"] = "com_zeapps_contact_read" ;
-$tabMenu["order"] = 40 ;
-$menuLeft[] = $tabMenu ;
 
 
 /************** insert in left menu ************/
-$tabMenu = array () ;
+/*$tabMenu = array () ;
 $tabMenu["id"] = "com_zeapps_statistics_liste_requetes";
 $tabMenu["space"] = "com_zeapps_statistics" ;
 $tabMenu["label"] = "Liste des requetes" ;
@@ -41,11 +23,11 @@ $tabMenu["fa-icon"] = "plus" ;
 $tabMenu["url"] = "/ng/com_zeapps_statistics/requetes_generees/new" ;
 $tabMenu["access"] = "com_zeapps_contact_read" ;
 $tabMenu["order"] = 0 ;
-$menuLeft[] = $tabMenu ;
+$menuLeft[] = $tabMenu ;*/
 
 
 /*************** insert in top menu ************/
-$tabMenu = array () ;
+/*$tabMenu = array () ;
 $tabMenu["id"] = "com_zeapps_statistics_liste_requetes" ;
 $tabMenu["space"] = "com_zeapps_statistics" ;
 $tabMenu["label"] = "Liste des requetes" ;
@@ -63,4 +45,4 @@ $tabMenu["fa-icon"] = "plus" ;
 $tabMenu["url"] = "/ng/com_zeapps_statistics/requetes_generees/new" ;
 $tabMenu["access"] = "com_zeapps_contact_read" ;
 $tabMenu["order"] = 0 ;
-$menuHeader[] = $tabMenu ;
+$menuHeader[] = $tabMenu ;*/
